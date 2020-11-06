f2387ce394e342a632ce2f4e7173de44a6d0c3c2
Axel Razafimahefa <axel.raza12@gmail.com>
2020-11-06 17:21:01 +0100

    Add home page

---
+++ js/home.js
@@ -0,0 +1,26 @@
+var init = function() {
+  loadStrawPolls();
+}
+
+var loadStrawPolls = function() {
+  addStrawpoll("Sondage 2", "test");
+  addStrawpoll("Sondage 3", "test 2");
+  addStrawpoll("Sondage 3", "test 2");
+  addStrawpoll("Sondage 3", "test 2");
+  addStrawpoll("Sondage 3", "test 2");
+}
+
+var addStrawpoll = function(title, description) {
+  $('#strawpolls')
+  .append('<div class="card">' +
+  '<div class="card-body">' +
+  '<h5 class="card-title">' + title + '</h5>' +
+  '<p class="card-text">' + description + '</p>' +
+  '<a href="#" class="btn btn-outline-dark">Voir</a>' +
+  '</div>' +
+  '</div>');
+}
+
+$(document).ready(function(){
+  init();
+});
